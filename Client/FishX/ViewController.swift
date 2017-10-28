7b19a8937a6b850f52e0b5e9713dd9193e48b578
Tayron <1652271127@qq.com>
2017-10-28 12:37:43 +0800

    客户端代码

---
+++ Client/FishX/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  FishX
+//
+//  Created by fang tairan on 2017/10/28.
+//  Copyright © 2017年 Tayron. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+import SDWebImage
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
